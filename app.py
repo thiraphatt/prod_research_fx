9dc135df7713b579facec2a2aacd7255c963436e
thiraphatt <thiraphat.t.su@gmail.com>
2022-03-24 05:55:48 +0700

    done

--- app.py
+++ app.py
@@ -20,22 +20,10 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.preprocessing import MinMaxScaler
 
 st.write("""
-# Fracture Prediction App
-Please input **paramerter** on your left!
+# 1-yr Mortality Prediction
 """)
 
-st.sidebar.header('User Input Parameters')
-#df.columns = ['Age', 'Sex','BMI','Preinjury_status','Preinjury_gaitaid','CKD', 'CCI','Heart_disease','CVA', 'Lung_disease', 'Dementia', 'Diagnosis','Type_of_operation','Status' , 'Time_to_op']
-#df #bedridden  indoordependent indoorindependent   outdoordependent    outdoorindependent
-#no ambulation  nogaitaid   quadcane    single cane tripod cane walker  wheelchair
-#Diagnosis intertroch   neck    subtroch    
-# Type_of_operation Cephalomedullary nailing    DHS Hemiarthroplasty    Multiple screw fixation THA conservative
-#Time_to_op >48hr
-#CKD
-#Heart_disease
-#CVA
-#Lung_disease
-#Dementia
+st.sidebar.header('Input Parameters')
 dataset_processed = pd.read_csv('dataset/dataset_processed.csv')
 dataset_processed_x = dataset_processed.iloc[:,:-1].values
 
@@ -92,7 +80,7 @@ def user_input_features():
     CCI = st.sidebar.slider(label='Charlson Comorbidity Index (CCI)', min_value=0, max_value=40, key=1)
     #CCI = st.sidebar.number_input("Charlson Comorbidity Index (CCI)", min_value=0, max_value=40, value=0)
 
-    Preinjury_status = st.sidebar.selectbox('How was Preinjury status?',('Outdoor independent','Outdoor dependent','Indoor independent','Indoor dependent','Bedridden'))
+    Preinjury_status = st.sidebar.selectbox('Pre-injury ambulatory status',('Outdoor independent','Outdoor dependent','Indoor independent','Indoor dependent','Bedridden'))
     bedridden = '0'
     indoordependent = '0'
     indoorindependent = '0'
@@ -110,7 +98,7 @@ def user_input_features():
         outdoorindependent = '1'
 
 
-    Preinjury_gaitaid = st.sidebar.selectbox('How was Preinjury gait aid?',('No gaitaid','Single cane','Tripod cane','Quad cane','Walker','Wheelchair', 'No ambulation'))
+    Preinjury_gaitaid = st.sidebar.selectbox('Assistive device',('No gaitaid','Single cane','Tripod cane','Quad cane','Walker','Wheelchair', 'No ambulation'))
 
 
     no_ambulation = '0'
@@ -140,7 +128,7 @@ def user_input_features():
     intertroch = '0'
     neck = '0'
     subtroch = '0'
-    Diagnosis = st.sidebar.selectbox('What is the Diagnosis?',('Intertrochanteric fracture','Femoral neck fracture','Subtrochanteric fracture'))
+    Diagnosis = st.sidebar.selectbox('Type of fracture',('Intertrochanteric fracture','Femoral neck fracture','Subtrochanteric fracture'))
     if Diagnosis =='Intertrochanteric fracture':
         intertroch = '1'
     elif Diagnosis =='Femoral neck fracture':
@@ -154,7 +142,7 @@ def user_input_features():
     Multiple_screw_fixation = '0'
     THA = '0'
     conservative = '0'
-    Type_of_operation = st.sidebar.selectbox('What is the type of operation?',('Multiple screw fixation', 'Cephalomedullary nailing','DHS','Hemiarthroplasty','THA','Conservative'))
+    Type_of_operation = st.sidebar.selectbox('Treatment',('Multiple screw fixation', 'Cephalomedullary nailing','DHS','Hemiarthroplasty','THA','Conservative treatment'))
     if Type_of_operation =='Cephalomedullary nailing':
         Cephalomedullary_nailing = '1'
     elif Type_of_operation =='DHS':
@@ -165,10 +153,10 @@ def user_input_features():
         Multiple_screw_fixation = '1'
     elif Type_of_operation =='THA':
         THA = '1'
-    elif Type_of_operation =='Conservative':
+    elif Type_of_operation =='Conservative treatment':
         conservative = '1'
 
-    Time_to_op = st.sidebar.radio("Time to operation", ['< 48 hr','> 48 hr'], index=1)
+    Time_to_op = st.sidebar.radio("Time to operation", ['<= 48 hr','> 48 hr'], index=1)
 
     if Time_to_op =='> 48 hr':
         M48hr = '1'
@@ -262,13 +250,20 @@ with open("Models/best_gbc.pickle", 'rb') as model:
 prediction = model.predict(df)
 prediction_proba = model.predict_proba(df)
 
+st.title('Prediction:')       
 
-#st.subheader('Class labels and their corresponding index number')
-#st.write(iris.target_names)
+prediction = str(prediction)
+if prediction == '[0]':
+    st.subheader('Prediction: Low risk of mortality')
+elif prediction == '[1]':
+    st.subheader('Prediction: Hign risk of mortality')
 
-st.subheader('Prediction')
-#st.write(iris.target_names[prediction])
-st.write(prediction)
+st.subheader('Probability of 1-yr mortality is ___%')
 
-st.subheader('Prediction Probability')
 st.write(prediction_proba)
+st.write(str(prediction_proba))
+
+
+
+
+st.write('DISCLAMER: The implementation of this 1-yr hip fracture mortality prediction is NOT intended for use supporting or informing clinical decision-making. It is ONLY to be used for academic research, peer review and validation purposes, and it must NOT be used with data or information relating to any individual.')
