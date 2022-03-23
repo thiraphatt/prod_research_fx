7caa0c8678d11f73144d1dc069744c6660ddb1ea
thiraphatt <thiraphat.t.su@gmail.com>
2022-03-23 08:42:57 +0700

    correct
    correct

--- app.py
+++ app.py
@@ -78,35 +78,36 @@ def user_input_features():
     Age = st.sidebar.number_input("Age", min_value=20, max_value=99, value=45)
     Sex = st.sidebar.radio("Gender", ['Male','Female'], index=1)
     if Sex == 'Male':
-        Male = '0'
-        Female = '1'
-    elif Sex == 'Female':
         Male = '1'
         Female = '0'
-    BMI = st.sidebar.slider('BMI', 16.00, 23.0, 30.00)
-
-    CCI = st.sidebar.slider('Charlson Comorbidity Index (CCI)', 0, 0, 40)
+    elif Sex == 'Female':
+        Male = '0'
+        Female = '1'
+    #BMI = st.sidebar.slider('BMI', 16.00, 23.0, 30.00)
+    BMI = st.sidebar.slider(label='BMI', min_value=16.00, max_value=35.00, key=21.0)
+    #CCI = st.sidebar.slider('Charlson Comorbidity Index (CCI)', 0, 0, 40)
+    CCI = st.sidebar.slider(label='Charlson Comorbidity Index (CCI)', min_value=0, max_value=40, key=1)
     #CCI = st.sidebar.number_input("Charlson Comorbidity Index (CCI)", min_value=0, max_value=40, value=0)
 
-    Preinjury_status = st.sidebar.selectbox('How was Preinjury status?',('bedridden','indoordependent','indoorindependent','outdoordependent','outdoorindependent'))
+    Preinjury_status = st.sidebar.selectbox('How was Preinjury status?',('Outdoor independent','Outdoor dependent','Indoor independent','Indoor dependent','Bedridden'))
     bedridden = '0'
     indoordependent = '0'
     indoorindependent = '0'
     outdoordependent = '0'
     outdoorindependent = '0'
-    if Preinjury_status =='bedridden':
+    if Preinjury_status =='Bedridden':
         bedridden = '1'
-    elif Preinjury_status =='indoordependent':
+    elif Preinjury_status =='Indoor dependent':
         indoordependent = '1'
-    elif Preinjury_status =='indoorindependent':
+    elif Preinjury_status =='Indoor independent':
         indoorindependent = '1'
-    elif Preinjury_status =='outdoordependent':
+    elif Preinjury_status =='Outdoor dependent':
         outdoordependent = '1'
-    elif Preinjury_status =='outdoorindependent':
+    elif Preinjury_status =='Outdoor independent':
         outdoorindependent = '1'
 
 
-    Preinjury_gaitaid = st.sidebar.selectbox('How was Preinjury gait aid?',('no ambulation','nogaitaid','quadcane','single cane','tripod cane','walker','wheelchair'))
+    Preinjury_gaitaid = st.sidebar.selectbox('How was Preinjury gait aid?',('No gaitaid','Single cane','Tripod cane','Quad cane','Walker','Wheelchair', 'No ambulation'))
 
 
     no_ambulation = '0'
@@ -116,19 +117,19 @@ def user_input_features():
     tripod_cane = '0'
     walker = '0'
     wheelchair = '0'
-    if Preinjury_gaitaid =='no ambulation':
+    if Preinjury_gaitaid =='No ambulation':
         no_ambulation = '1'
-    elif Preinjury_status =='nogaitaid':
+    elif Preinjury_status =='No gaitaid':
         nogaitaid = '1'
-    elif Preinjury_status =='quadcane':
+    elif Preinjury_status =='Quad cane':
         quadcane = '1'
-    elif Preinjury_status =='single cane':
+    elif Preinjury_status =='Single cane':
         single_cane = '1'
-    elif Preinjury_status =='tripod cane':
+    elif Preinjury_status =='Tripod cane':
         tripod_cane = '1'
-    elif Preinjury_status =='walker':
+    elif Preinjury_status =='Walker':
         walker = '1'
-    elif Preinjury_status =='wheelchair':
+    elif Preinjury_status =='Wheelchair':
         wheelchair = '1'
 
 
@@ -136,12 +137,12 @@ def user_input_features():
     intertroch = '0'
     neck = '0'
     subtroch = '0'
-    Diagnosis = st.sidebar.selectbox('What is the Diagnosis?',('intertroch','neck','subtroch'))
-    if Diagnosis =='intertroch':
+    Diagnosis = st.sidebar.selectbox('What is the Diagnosis?',('Intertrochanteric fracture','Femoral neck fracture','Subtrochanteric fracture'))
+    if Diagnosis =='Intertrochanteric fracture':
         intertroch = '1'
-    elif Diagnosis =='neck':
+    elif Diagnosis =='Femoral neck fracture':
         neck = '1'
-    elif Diagnosis =='subtroch':
+    elif Diagnosis =='Subtrochanteric fracture':
         subtroch = '1'
 
     Cephalomedullary_nailing = '0'
@@ -150,7 +151,7 @@ def user_input_features():
     Multiple_screw_fixation = '0'
     THA = '0'
     conservative = '0'
-    Type_of_operation = st.sidebar.selectbox('What is the type of operation?',('Cephalomedullary nailing','DHS','Hemiarthroplasty','Multiple screw fixation','THA','conservative'))
+    Type_of_operation = st.sidebar.selectbox('What is the type of operation?',('Multiple screw fixation', 'Cephalomedullary nailing','DHS','Hemiarthroplasty','THA','Conservative'))
     if Type_of_operation =='Cephalomedullary nailing':
         Cephalomedullary_nailing = '1'
     elif Type_of_operation =='DHS':
@@ -161,10 +162,10 @@ def user_input_features():
         Multiple_screw_fixation = '1'
     elif Type_of_operation =='THA':
         THA = '1'
-    elif Type_of_operation =='conservative':
+    elif Type_of_operation =='Conservative':
         conservative = '1'
 
-    Time_to_op = st.sidebar.radio("Time to op", ['< 48 hr','> 48 hr'], index=1)
+    Time_to_op = st.sidebar.radio("Time to operation", ['< 48 hr','> 48 hr'], index=1)
 
     if Time_to_op =='> 48 hr':
         M48hr = '1'
@@ -178,7 +179,7 @@ def user_input_features():
     else:
         CKD = '0'
 
-    Heart_disease = st.sidebar.checkbox("Heart_disease")
+    Heart_disease = st.sidebar.checkbox("Heart disease")
     if Heart_disease:
         Heart_disease = '1'
     else:
@@ -188,7 +189,7 @@ def user_input_features():
         CVA = '1'
     else:
         CVA = '0'
-    Lung_disease = st.sidebar.checkbox("Lung_disease")
+    Lung_disease = st.sidebar.checkbox("Lung disease")
     if Lung_disease:
         Lung_disease = '1'
     else:
@@ -234,23 +235,33 @@ def user_input_features():
                 ">48hr": M48hr
             }
     
-    features = pd.DataFrame(data, index=[0])
+    
 
-    return features
-df = user_input_features()
+    return data
+data = user_input_features()
+df = pd.DataFrame(data, index=[0])
 
                                                         
 st.subheader('Input parameters')
+#st.write(data)
+
 st.write(df)
 
 
 with open("Models/best_gbc.pickle", 'rb') as model:
     model = pickle.load(model)
 
+
+scaler = MinMaxScaler()
+scaler.fit(df)
+df = scaler.transform(df)
+df = df
+
 prediction = model.predict(df)
 prediction_proba = model.predict_proba(df)
 
-st.subheader('Class labels and their corresponding index number')
+
+#st.subheader('Class labels and their corresponding index number')
 #st.write(iris.target_names)
 
 st.subheader('Prediction')
