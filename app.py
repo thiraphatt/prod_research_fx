4b441ab5cf0962147674296440011269fad97154
thiraphatt <thiraphat.t.su@gmail.com>
2022-05-28 07:22:06 +0700

    gg js

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import pickle
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
+
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
@@ -298,3 +303,38 @@ st.write(text_prob)
 st.write('') 
 st.write('') 
 st.write('DISCLAMER: The implementation of this 1-yr hip fracture mortality prediction is NOT intended for use supporting or informing clinical decision-making. It is ONLY to be used for academic research, peer review and validation purposes, and it must NOT be used with data or information relating to any individual.')
+
+
+def inject_ga():
+    GA_ID = "google_analytics"
+
+    # Note: Please replace the id from G-PELKBJ6ES5 to whatever your
+    # web application's id is. You will find this in your Google Analytics account
+    
+    GA_JS = """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PELKBJ6ES5"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-PELKBJ6ES5');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
+inject_ga()
